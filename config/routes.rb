a23fb9d38a808173e4e47dfe8cc8bb60b4b752e0
John <jsrratt@aol.com>
2013-12-21 16:36:04 -0500

    Temp

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Bloccit::Application.routes.draw do
-  get "welcome/index"
 
-  get "welcome/about"
+  resources :posts
 
-  root to: 'welcome#index'
+  match "about" => 'welcome#about', via: :get
 
+  root :to => 'welcome#index'
+end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -61,4 +62,4 @@ Bloccit::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
+
