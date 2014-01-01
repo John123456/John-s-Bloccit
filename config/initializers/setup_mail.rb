ee39eb663ab8c987b47632086735f3b5882331c9
John <jsrratt@aol.com>
2014-01-01 15:38:45 -0500

    Associated users and posts

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,7 +4,7 @@ if Rails.env.development?
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => app20351784@heroku.com,
+    :user_name      => 'app20351784@heroku.com',
     :password       => 'f10oxf92',
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
