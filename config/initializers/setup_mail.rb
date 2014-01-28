f4321bb006bd7ebf038ed6ee38da6e74b85895b2
John <jsrratt@aol.com>
2014-01-27 21:36:15 -0500

    Jan27th

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,8 @@ if Rails.env.development?
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'app20351784@heroku.com',
-    :password       => 'f10oxf92',
+    :user_name      => ENV['SENGRID_USERNAME'],
+    :password       => ENV['SENGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
